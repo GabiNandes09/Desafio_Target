ffa57b20cb1463bc5bbda9ebc7abe924c407f4b9
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2024-03-12 22:26:44 -0300

    Desafios completos

---
+++ Exer_5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exer_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entre com a string desejada: ");
+        String s = sc.nextLine();
+        String stringInvertida = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+			stringInvertida += s.charAt(i);
+		}
+		System.out.println(stringInvertida);
+
+
+    }
+}
